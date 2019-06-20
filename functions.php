7778d6441068a2f1fa0d6b5ed157f8b3637388be
Isabella Chen <miss.izzyc@gmail.com>
2019-06-20 22:16:36 +0200

    add style scaffolding inc google fonts

--- functions.php
+++ functions.php
@@ -131,7 +131,7 @@ function lettra_scripts()
 
   wp_enqueue_script('lettra-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true);
 
-  wp_enqueue_style('googlefont_css', 'https://fonts.googleapis.com/css?family=Lato:300,300i,400|Roboto+Slab:300,400,700&display=swap');
+  wp_enqueue_style('googlefont_css', 'https://fonts.googleapis.com/css?family=Lato:300,300i,400|Roboto+Slab:100,300,400,700&display=swap');
   if (is_singular() && comments_open() && get_option('thread_comments')) {
     wp_enqueue_script('comment-reply');
   }
